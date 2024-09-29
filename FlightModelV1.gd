07c3bc85ecc0b2de1571b7ab848e54651b4842a6
DESKTOP-IKVA0N7\Durandal <danielmannnet@protonmail.com>
2024-09-29 16:22:26 -0700

    Busted

--- FlightModelV1.gd
+++ FlightModelV1.gd
@@ -24,6 +24,7 @@ var direct_control = false
 
 @export_group("Aerodynamics")
 @export var AoA_to_lift : Array[Vector2] = []
+@export var AoA_to_lift_elivator : Array[Vector2] = []
 @export var max_thrust : float = 0
 @export var angle_of_incidence : float = 0
 @export var wing_area : float = 1
@@ -41,8 +42,6 @@ var direct_control = false
 
 @export var roll_stabilizer_multiplier : float = 1
 
-@export var elevator_max_lift_coeficient : float = 1.0
-@export var elevator_min_lift_coeficient : float = -1.0
 @export var elevator_force_multiplier : float = 1
 @export var aileron_max_angle : float = 5
 
@@ -84,12 +83,12 @@ func process_input(delta : float) -> void:
 func aerodynamic_update(delta : float):
 	# Thrust Calculations
 	var thrust_magnitude = max_thrust * throttle
-	var thrust : Vector3 = transform.basis.z.normalized() * thrust_magnitude
+	var thrust : Vector3 = basis.z.normalized() * thrust_magnitude
 	apply_central_force(thrust)
 	
 	update_wings()
-	update_stabilizers()
-	#update_elevators()
+	update_horizontal_stabilizers()
+	
 	var pitch_velocity = angular_velocity.dot(basis.x)
 	HUD_Speed.text = "Speed: %f, Pitch Vel: %f" % [linear_velocity.length(), rad_to_deg(pitch_velocity)]
 	HUD_Alt.text = "Alt: %f, Pitch %f" % [position.y, rad_to_deg(rotation.x)]
@@ -97,11 +96,11 @@ func aerodynamic_update(delta : float):
 # These calculations are not truely accurate. They provide stabilization with no genuine airframe drag
 func update_stabilizers():
 	# vertical
-	var local_velocity = linear_velocity * basis.orthonormalized()
-	var vertical_velocity = Vector3(local_velocity.x,0,local_velocity.z)
-	var vertical_drag_coefficient = vertical_velocity.normalized().dot(Vector3(1,0,0))
-	var vertical_drag_torque = vertical_drag_coefficient * pow(vertical_velocity.dot(Vector3(1,0,0)), 2) * vertical_stabilizer_multiplier
-	apply_torque(basis.orthonormalized().y.normalized() * vertical_drag_torque)
+	#var local_velocity = linear_velocity * basis.orthonormalized()
+	#var vertical_velocity = Vector3(local_velocity.x,0,local_velocity.z)
+	#var vertical_drag_coefficient = vertical_velocity.normalized().dot(Vector3(1,0,0))
+	#var vertical_drag_torque = vertical_drag_coefficient * pow(vertical_velocity.dot(Vector3(1,0,0)), 2) * vertical_stabilizer_multiplier
+	#apply_torque(basis.orthonormalized().y.normalized() * vertical_drag_torque)
 	
 	# horizontal
 	# var horizontal_velocity = Vector3(0,local_velocity.y,local_velocity.z)
@@ -122,29 +121,38 @@ func update_elevators():
 	local_velocity.x = 0
 	
 	# lift calculation
-	var lift_coefficient = remap(elevator, -1, 1, elevator_min_lift_coeficient, elevator_max_lift_coeficient)
-	var lift_torque = lift_coefficient * local_velocity.length_squared() * elevator_force_multiplier
-	apply_torque(basis.orthonormalized().x.normalized() * -lift_torque)
+	#var lift_coefficient = remap(elevator, -1, 1, elevator_min_lift_coeficient, elevator_max_lift_coeficient)
+	#var lift_torque = lift_coefficient * local_velocity.length_squared() * elevator_force_multiplier
+	#apply_torque(basis.orthonormalized().x.normalized() * -lift_torque)
 
 func update_horizontal_stabilizers():
 	var local_velocity = linear_velocity * basis.orthonormalized()
 	local_velocity.x = 0
 	
 	var AoA_sign = -signf(local_velocity.y)
-	var AoA = ( rad_to_deg(local_velocity.angle_to(Vector3(0,0,1))) * AoA_sign ) + angle_of_incidence
+	var AoA = ( rad_to_deg(local_velocity.angle_to(Vector3(0,0,1))) * AoA_sign )
 	
-	# lift calculation
-	var lift_coefficient = remap(AoA + (elevator * 3), -15, 15, -1.2, 1.2)
-	var lift_force = lift_coefficient * local_velocity.length_squared() * elevator_force_multiplier
-	apply_force(Vector3(0,1,0) * lift_force, basis.z * -5)
-	#apply_torque(basis.orthonormalized().x.normalized() * -lift_torque)
+	# lift 
+	#var lift_coefficient = remap(AoA - (elevator * 1), -15, 15, -1.0, 1.0)
+	#var lift_force = lift_coefficient * local_velocity.length_squared() * elevator_force_multiplier
+	#var local_lift_normal = local_velocity.normalized().cross(Vector3(1,0,0)).normalized() 
+	#apply_force((basis.orthonormalized() * local_lift_normal) * lift_force, basis.z * -5)
+	
+	#var lift_coefficient = clampf(remap(AoA - (elevator * 1), -15, 15, -1.0, 1.0),-1.0,1.0)
+	var lift_coefficient : float = read_data(AoA + elevator, AoA_to_lift_elivator)
+	#var lift_force = lift_coefficient * local_velocity.length_squared() * elevator_force_multiplier
+	#var local_lift_normal = local_velocity.normalized().cross(Vector3(1,0,0)).normalized() 
+	#apply_force((basis.orthonormalized() * local_lift_normal) * lift_force, basis.z * -5)
+	
+	
+	var local_lift_normal = local_velocity.normalized().cross(Vector3(1,0,0)).normalized() 
+	apply_force((basis.orthonormalized() * local_lift_normal) * lift_coefficient * local_velocity.length_squared() * elevator_force_multiplier, basis.z * -1.0)
 
-	# horizontal
-	var horizontal_velocity = Vector3(0,local_velocity.y,local_velocity.z)
-	var horizontal_drag_coefficient = horizontal_velocity.normalized().dot(Vector3(0,1,0))
-	var horizontal_drag_torque = -1.0 * horizontal_drag_coefficient * horizontal_velocity.length_squared() * horizontal_stabilizer_multiplier
-	apply_torque(basis.orthonormalized().x.normalized() * horizontal_drag_torque)
-	HUD_lift.text += "HozStab Torque %f" % horizontal_drag_torque
+	# drag
+	var horizontal_drag_coefficient = absf(local_velocity.normalized().dot(Vector3(0,1,0)))
+	var horizontal_drag_force = horizontal_drag_coefficient * local_velocity.length_squared() * horizontal_stabilizer_multiplier
+	apply_force(-1.0 * linear_velocity.normalized() * horizontal_drag_force, basis.z * -1)
+	HUD_lift.text += "HozStab Torque %f" % horizontal_drag_force
 
 func update_wings():
 	# Lift Calculations
@@ -163,7 +171,7 @@ func update_wings():
 	
 	var lift_coefficient_left = read_data(wing_AoA + aileron_angle_left, AoA_to_lift)
 	var lift_coefficient_right = read_data(wing_AoA + aileron_angle_right, AoA_to_lift)
-	var drag_coefficient = Vector3(0,1,0).dot(wing_local_velocity.normalized())
+	var drag_coefficient = absf(Vector3(0,1,0).dot(wing_local_velocity.normalized()))
 	
 	# Bernoulli equation
 	var lift_force_left = lift_coefficient_left * wing_lift_multiplier * wing_local_velocity.length_squared() * 0.5 * wing_area
@@ -173,9 +181,10 @@ func update_wings():
 	var wing_drag_force = drag_coefficient * wing_drag_multiplier * wing_local_velocity.length_squared() * 0.5 * wing_area
 	
 	HUD_lift.text = "Lift: %f, Torque %f" % [wing_lift_force, roll_torque]
-	
+	# lift is perpendicular to the flow
+	var local_lift_normal = wing_local_velocity.normalized().cross(Vector3(1,0,0)).normalized()
 	# TODO is lift always perpendicular to the aircraft or is it perpendicular to the wing?>
-	apply_central_force(wing_lift_force * basis.y.normalized())
+	apply_central_force(wing_lift_force * (basis.orthonormalized() * local_lift_normal))
 	apply_central_force(wing_drag_force * (basis.orthonormalized() * wing_local_velocity).normalized() * -1.0)
 	apply_torque(basis.z.normalized() * roll_torque)
 
